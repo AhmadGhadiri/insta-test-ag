6e6ab346b8d9e9c0023b333c3ec8244fbef6db45
Ahmad Ghadiri <ahmad.ghadiri@fireeye.com>
2022-02-07 14:09:25 -0500

    Added the post photos

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,6 +13,20 @@
                 <div class="ps-3"><strong>23k</strong> followers</div>
                 <div class="ps-3"><strong>212</strong> following</div>
             </div>
+            <div class="pt-4 font-weight-bold"><strong>freeCodeCamp.org</strong></div>
+            <div>We're a community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</div>
+            <div><a href="www.freecodecamp.org" class="text-decoration-none link-secondary">www.freecodecamp.org</a></div>
+        </div>
+    </div>
+    <div class="row pt-4">
+        <div class="col-4">
+            <img src="https://www.freecodecamp.org/news/content/images/size/w2000/2021/07/maxresdefault--16-.jpeg" class="w-100 h-200">
+        </div>
+        <div class="col-4">
+            <img src="https://www.freecodecamp.org/news/content/images/size/w2000/2021/07/maxresdefault--16-.jpeg" class="w-100 h-100">
+        </div>
+        <div class="col-4">
+            <img src="https://www.freecodecamp.org/news/content/images/size/w2000/2021/07/maxresdefault--16-.jpeg" class="w-100 h-100">
         </div>
     </div>
 </div>
